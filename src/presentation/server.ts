1b912e01d7ca8482c932b3b50992bcd520f68697
Bryan-Escobar <bryan.escobar2@catolica.edu.sv>
2025-01-14 15:29:17 -0600

    Revert "opcion de publicpath realizado"
    This reverts commit 62142474bde4967e72bd034b86e7c446df69a547.

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 interface Options {
     port: number;
-    publicPath?: string;
+    publicPath: string;
 }
 export class Server {
     private app = express();
@@ -10,9 +10,8 @@ export class Server {
     private readonly publicPath: string;
     constructor(private options: Options) 
     {
-        const {port, publicPath='public'} = options;
-        this.port = port;
-        this.publicPath = publicPath;
+        this.port = options.port;
+        this.publicPath = options.publicPath;
     }
 
     async start() {

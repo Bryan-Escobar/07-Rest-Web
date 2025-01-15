cdc09c7644436b6ff5231ab80afc2fe5717e71d1
Bryan-Escobar <bryan.escobar2@catolica.edu.sv>
2025-01-15 17:28:56 -0600

    fin de restful api

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 const todos = [
-    { id: 1, text: 'buy milk', createdAt: new Date() },
-    { id: 2, text: 'buy bread', createdAt: null },
+    { id: 1, text: 'buy milk', completedAt: new Date() },
+    { id: 2, text: 'buy bread', completedAt: null },
 ];
 export class TodosController {
     //* DI (dependency injection)
@@ -14,11 +14,9 @@ export class TodosController {
     }
     public getTodoById = (req: Request, res: Response) => {
         const id = +req.params.id;
-        if (isNaN(id)) {
-            res.status(400).json({ message: 'id must be a number' });
+        if (!this.isIdValid(id,res)) {
             return;
         }
-
         const todo = todos.find(todo => todo.id === id);
         if (!todo) {
             res.status(404).json({ message: `todo with id ${id} not found` });
@@ -29,18 +27,67 @@ export class TodosController {
 
     }
     public createTodo = (req: Request, res: Response) => {
-        const {text}=req.body;
-        if(text)
-        {
+        const { text } = req.body;
+        if (text) {
 
-            const newTodo={id:todos.length+1,text:text,createdAt:null};
+            const newTodo = { id: todos.length + 1, text: text, completedAt: null };
             todos.push(newTodo);
             res.status(201).json(newTodo);
         }
-        res.status(400).json({message:'text is required'});
+        res.status(400).json({ message: 'text is required' });
         return;
     }
     public updateTodo = (req: Request, res: Response) => {
-        res.json({ message: 'update todo' });
+        const id = +req.params.id;
+        if (!this.isIdValid(id,res)) {
+            
+            return;
+        }
+        const todo = todos.find(todo => todo.id === id);
+        if (!todo) {
+            res.status(404).json({ message: `todo with id ${id} not found` });
+            return;
+        }
+        const { text,createdAt: completedAt } = req.body;
+
+        // if (!text) {
+        //     res.status(400).json({ message: 'text is required' });
+        //     return;
+        // }
+        todo.text = text || todo.text;
+        if (completedAt===null) {
+            todo.completedAt=null;
+        }
+        else{
+            todo.completedAt = new Date(completedAt || todo.completedAt);
+        }
+        //! OJO, en js los objetos son pasados por referencia
+        
+        res.json(todo);
+        return;
+       
+        
+    }
+    public deleteTodo = (req: Request, res: Response) => {
+        const id=+req.params.id;
+        if (!this.isIdValid(id,res)) {
+            return;
+        }
+        const index=todos.findIndex(todo=>todo.id===id);
+        if (index===-1)
+        {
+            res.status(404).json({message:`todo with id ${id} not found`});
+            return;
+        }
+        res.json(todos[index]);
+        todos.splice(index,1);
+    }
+    isIdValid=(id:number,res:Response)=>{
+        if (isNaN(id)) {
+            res.status(400).json({ message: 'id must be a number' });
+            return false;
+        }
+        return true;
+
     }
 }
--- src/presentation/todos/todo-routes.ts
+++ src/presentation/todos/todo-routes.ts
@@ -17,6 +17,8 @@ export class TodosRoutes
 
         router.get('/:id',todoController.getTodoById);
         router.post('/',todoController.createTodo);
+        router.put('/:id',todoController.updateTodo);
+        router.delete('/:id',todoController.deleteTodo);
         return router;
     }
 }

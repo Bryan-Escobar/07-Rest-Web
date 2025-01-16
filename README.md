9d873ac81a40da66b75d610b29aeaa34812f42ba
Bryan-Escobar <bryan.escobar2@catolica.edu.sv>
2025-01-16 15:49:32 -0600

    CRUD con immplementacion basica de postgres

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
+# DEV
 Pasos para levantar la aplicacion.
 1.ejecutar npm install para instalar los node_modules
 2.Crear el .env apartir del .env.template
 3.generar el certificado para https con: openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout server.key -out server.crt
 4.ejecutar npm run dev
+5.ejecutar ```docker compose up -d```  para levantar el contenedor de docker 
--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,23 +1,22 @@
 import { Request, Response } from "express";
-const todos = [
-    { id: 1, text: 'buy milk', completedAt: new Date() },
-    { id: 2, text: 'buy bread', completedAt: null },
-];
+import { prisma } from "../../data/postgres";
 export class TodosController {
     //* DI (dependency injection)
     constructor() {
 
     }
 
-    public getTodos = (req: Request, res: Response) => {
+    public getTodos = async (req: Request, res: Response) => {
+        const todos = await prisma.todo.findMany();
         res.json(todos);
     }
-    public getTodoById = (req: Request, res: Response) => {
+    public getTodoById = async (req: Request, res: Response) => {
         const id = +req.params.id;
-        if (!this.isIdValid(id,res)) {
+        if (!this.isIdValid(id, res)) {
             return;
         }
-        const todo = todos.find(todo => todo.id === id);
+        const todo = await this.findTodoById(id);
+
         if (!todo) {
             res.status(404).json({ message: `todo with id ${id} not found` });
             return;
@@ -26,68 +25,96 @@ export class TodosController {
         res.json(todo);
 
     }
-    public createTodo = (req: Request, res: Response) => {
-        const { text } = req.body;
+    public createTodo = async (req: Request, res: Response) => {
+        const { text, completedAt } = req.body;
+        let newTodo = null
         if (text) {
+            if (completedAt) {
 
-            const newTodo = { id: todos.length + 1, text: text, completedAt: null };
-            todos.push(newTodo);
+                newTodo = await prisma.todo.create({
+                    data: { text, completedAt }
+                })
+            }
+            else {
+                newTodo = await prisma.todo.create({
+                    data: { text }
+                })
+            }
+
             res.status(201).json(newTodo);
+            return;
         }
         res.status(400).json({ message: 'text is required' });
         return;
     }
-    public updateTodo = (req: Request, res: Response) => {
+    public updateTodo = async (req: Request, res: Response) => {
         const id = +req.params.id;
-        if (!this.isIdValid(id,res)) {
-            
+        if (!this.isIdValid(id, res)) {
+
             return;
         }
-        const todo = todos.find(todo => todo.id === id);
+        const todo = await this.findTodoById(id);
+
         if (!todo) {
             res.status(404).json({ message: `todo with id ${id} not found` });
             return;
         }
-        const { text,createdAt: completedAt } = req.body;
 
-        // if (!text) {
-        //     res.status(400).json({ message: 'text is required' });
-        //     return;
-        // }
-        todo.text = text || todo.text;
-        if (completedAt===null) {
-            todo.completedAt=null;
+        //validaciones de los datos enviados
+        let { text, completedAt } = req.body;
+        if (!completedAt) { //si no se envia completedAt, se mantiene el valor actual
+            completedAt = todo.completedAt;
         }
-        else{
-            todo.completedAt = new Date(completedAt || todo.completedAt);
+        if (isNaN(new Date(completedAt).getTime())) { //verifica que completedAt sea una fecha valida
+
+            res.status(400).json({ message: 'completedAt must be a valid date' });
+            console.log('no es una fecha valida');
+            return
         }
-        //! OJO, en js los objetos son pasados por referencia
-        
-        res.json(todo);
+
+        //actualizacon de los datos
+        const updatedTodo = await prisma.todo.update({
+            where: { id: id },
+            data: { text: text, completedAt: completedAt }
+        });
+        res.json(updatedTodo);
         return;
-       
-        
+
+
     }
-    public deleteTodo = (req: Request, res: Response) => {
-        const id=+req.params.id;
-        if (!this.isIdValid(id,res)) {
+    public deleteTodo = async (req: Request, res: Response) => {
+        const id = +req.params.id;
+        if (!this.isIdValid(id, res)) {
             return;
         }
-        const index=todos.findIndex(todo=>todo.id===id);
-        if (index===-1)
-        {
-            res.status(404).json({message:`todo with id ${id} not found`});
+
+
+        try {
+            const todo = await prisma.todo.delete({
+                where: { id: id }
+            });
+            res.json(todo);
+            return;
+        } catch (error) {
+
+            res.status(404).json({ message: `todo with id ${id} not found` });
             return;
         }
-        res.json(todos[index]);
-        todos.splice(index,1);
+
     }
-    isIdValid=(id:number,res:Response)=>{
+
+
+    isIdValid = (id: number, res: Response) => {
         if (isNaN(id)) {
             res.status(400).json({ message: 'id must be a number' });
             return false;
         }
         return true;
 
+    }
+    findTodoById = (id: number) => {
+        return prisma.todo.findUnique({
+            where: { id: id }
+        });
     }
 }

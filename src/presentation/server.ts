cfd57f156f41d2b4c5e28ea9acf23df48e4128b2
Bryan-Escobar <bryan.escobar2@catolica.edu.sv>
2025-01-23 11:40:37 -0600

    fin seccion 16

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,6 @@
 import express, { Router } from 'express';
 import path from 'path';
+import compression from 'compression';
 interface Options {
     port: number;
     routes:Router;
@@ -24,6 +25,7 @@ export class Server {
         //* Middlewares
         this.app.use(express.json()); //indica que se va a recibir informacion en formato json
         this.app.use(express.urlencoded({ extended: true })); //indica que se va a recibir informacion en formato urlencoded
+        this.app.use(compression()); //indica que se va a comprimir la informacion que se envia al cliente
         //* Public Folder
         this.app.use(express.static(this.publicPath));
         //indica que se va a enviar informacion de la carpeta public

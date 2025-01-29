f27b91fb33449f87fcb817d86b58f2777a0df394
Bryan-Escobar <bryan.escobar2@catolica.edu.sv>
2025-01-29 16:40:53 -0600

    new testing for api

--- tests/presentation/todos/todo-routes.test.ts
+++ tests/presentation/todos/todo-routes.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { testServer } from '../../test-server';
 import { prisma } from '../../../src/data/postgres';
 import { response } from 'express';
+import exp from 'constants';
 beforeAll(async ()=>
 {
     await testServer.start();
@@ -38,11 +39,41 @@ describe('Testing todo-routes.ts', () => {
 
     });
     test('should return a 404 not found api/todos/:id', async() => {
+        const id=999;
         const {body}=await request(testServer.app)
-        .get('/api/todos/999')
+        .get('/api/todos/'+id)
         .expect(400);
-        expect(body).toEqual('Todo with id 999 not found');
+        expect(body).toEqual(`Todo with id ${id} not found`);
     });
-
+    test('should return a new Todo', async() => {
+        const response=await request(testServer.app)
+        .post('/api/todos')
+        .send(todo1)
+        .expect(201);
+        console.log(response.body);
+        expect(response.body).toEqual({id:expect.any(Number),text:todo1.text, completedAt:null});
+    });
     
+    test('should return a new updated TODO api/todos/:id', async() => {
+        const todo=await prisma.todo.create({
+            data:todo1
+        });
+        const {body}=await request(testServer.app)
+        .put(`/api/todos/${todo.id}`)
+        .send({text:'hola mundo', completedAt:'2023-10-21'})
+        .expect(400);
+        expect(body).toEqual({id:expect.any(Number),text:'hola mundo', completedAt:'2023-10-21'});
+    });
+    
+
+    test('should delete a todo api/todos:id', async() => {
+        const todo=await prisma.todo.create({
+            data:todo1
+        });
+        const {body}=await request(testServer.app)
+        .delete(`/api/todos/${todo.id}`)
+        .expect(200);
+        console.log(body);
+        expect(body).toEqual({ id: expect.any(Number), text: 'todo1', completedAt: null })
+    });
 });
